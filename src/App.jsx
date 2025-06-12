fe4cd7d60e3dd0def1ff5c56807e5f76466273bc
Uchenna ogbu <Ucheogbu0070@gmail.com>
2025-06-12 04:03:29 +0300

    Navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import Navbar from "./Components/Navbar";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div>
-      <h1>Hellow world</h1>
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 }
 
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const Navbar = () => {
+  return (
+    <>
+      <header>
+        <nav>
+          <ul>
+            <li>HOME</li>
+            <li>ABOUT</li>
+            <li>MENU</li>
+            <li>RESERVATION</li>
+            <li>ONLINE-ORDER</li>
+            <li>LOGIN</li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+};
