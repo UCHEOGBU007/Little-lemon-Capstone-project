397a9df4caf20b5282786b26874c58ff6ab4a5f9
Uchenna ogbu <Ucheogbu0070@gmail.com>
2025-06-12 04:11:18 +0300

    herosection added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Navbar from "./Components/Navbar";
+import Herosection from "./Components/Herosection";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Herosection />
     </>
   );
 }
