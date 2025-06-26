21b4216ce8225795b5bb1426b1531eb69b1036a9
Uchenna ogbu <Ucheogbu0070@gmail.com>
2025-06-26 09:26:54 +0300

    like

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,12 +1,67 @@
+// import React from "react";
+// import { BrowserRouter, Router, Routes, Route, Link } from "react-router-dom";
+// import App from "../App.jsx";
+
+// import Menu from "./src/Components/Menu.jsx";
+// import About from "./src/Components/Aboutjsx";
+// import Contact from "./src/Components/Contact.jsx";
+// import Order from "./src/Components/Order-online.jsx";
+// import Reservation from "./src/Components/Reservation.jsx";
+
+// const Navigation = () => {
+//   return (
+//     <Router>
+//       <nav>
+//         <ul>
+//           <li>
+//             {" "}
+//             <Link to="/Home">HOME </Link>
+//           </li>
+
+//           <li>
+//             <Link to="/About">ABOUT</Link>
+//           </li>
+
+//           <li>
+//             <Link to="/Contact-us">CONTACT US</Link>
+//           </li>
+
+//           <li>
+//             <Link to="/Order-online">ORDER-ONLINE</Link>
+//           </li>
+
+//           <li>
+//             <Link to="/Menu">MENU</Link>
+//           </li>
+
+//           <li>
+//             <Link to="/Reservation">RESERVATION</Link>
+//           </li>
+//         </ul>
+//       </nav>
+//       <Routes>
+//         <Route path="/Home" element={<App />} />
+//         <Route path="/About" element={<About />} />
+//         <Route path="/Contact-us" element={<Contact />} />
+//         <Route path="/Order-online" element={<Order />} />
+//         <Route path="/Menu" element={<Menu />} />
+//         <Route path="/Reservation" element={<Reservation />} />
+//       </Routes>
+//     </Router>
+//   );
+// };
+
+// export default Navigation;
+
 import React from "react";
-import { BrowserRouter, Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import App from "../App.jsx";
 
-import Menu from "./Components/Menu.jsx";
-import About from "./Components/Aboutjsx";
-import Contact from "./Components/Contact.jsx";
-import Order from "./Components/Order-online.jsx";
-import Reservation from "./Components/Reservation.jsx";
+import Menu from "./Menu.jsx";
+import About from "./Aboutjsx";
+import Contact from "./Contact.jsx";
+import Order from "./Order-online.jsx";
+import Reservation from "./Reservation.jsx";
 
 const Navigation = () => {
   return (
@@ -14,31 +69,26 @@ const Navigation = () => {
       <nav>
         <ul>
           <li>
-            {" "}
-            <Link to="/">HOME </Link>
+            <Link to="/">HOME</Link>
           </li>
-
           <li>
-            <Link to="/ABOUT">ABOUT</Link>
+            <Link to="/about">ABOUT</Link>
           </li>
-
           <li>
-            <Link to="/CONTACT US">CONTACT US</Link>
+            <Link to="/contact-us">CONTACT US</Link>
           </li>
-
           <li>
-            <Link to="/ORDER-ONLINE">ORDER-ONLINE</Link>
+            <Link to="/order-online">ORDER ONLINE</Link>
           </li>
-
           <li>
-            <Link to="/MENU">MENU</Link>
+            <Link to="/menu">MENU</Link>
           </li>
-
           <li>
-            <Link to="/RESERVATION">RESERVATION</Link>
+            <Link to="/reservation">RESERVATION</Link>
           </li>
         </ul>
       </nav>
+
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/about" element={<About />} />
