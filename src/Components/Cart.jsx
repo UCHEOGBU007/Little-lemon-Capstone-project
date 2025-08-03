46f297e5f5c0bc57ab3ad2cfa4a086d4b4c8b2bc
Uchenna ogbu <Ucheogbu0070@gmail.com>
2025-08-03 14:46:43 +0300

    Adding cart styles

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,6 +1,7 @@
 // src/components/Cart.jsx
 import { useContext } from "react";
 import { CartContext } from "./ContextApi";
+import Style from "../Css/Cart.module.css";
 
 const Cart = () => {
   const {
@@ -14,22 +15,33 @@ const Cart = () => {
 
   return (
     <>
-      <div>
-        <h2>Your Cart</h2>
-        {cartItems.map((item) => (
-          <div key={item.id}>
-            {item.name} - ${item.price.value} × {item.quantity} = $
-            {item.price.value * item.quantity}{" "}
-            <button onClick={() => decreaseQuantity(item.id)}>-</button>
-            {item.quantity}
-            <button onClick={() => increaseQuantity(item.id)}>+</button>
-            <button onClick={() => removeFromCart(item.id)}>Remove</button>
-          </div>
-        ))}
+      <section className={Style.cartcontainer}>
+        <div className={Style.cart}>
+          <h2>Items Selected</h2>
+          {cartItems.map((item) => (
+            <div key={item.id} className={Style.cartitems}>
+              {item.name} - ${item.price.value}× {item.quantity}= $
+              {item.price.value * item.quantity}{" "}
+              {/* span for Buttons increament and decreament */}
+              <span className={Style.cartquantity}>
+                <button onClick={() => decreaseQuantity(item.id)}>-</button>
+                {item.quantity}
+                <button onClick={() => increaseQuantity(item.id)}>+</button>
+                <button onClick={() => removeFromCart(item.id)}>Remove</button>
+              </span>
+            </div>
+          ))}
 
-        <button onClick={clearCart}>Clear Cart</button>
-      </div>
-      <h3>Total: ${getTotalPrice()}</h3>
+          <button onClick={clearCart}>Clear Cart</button>
+        </div>
+
+        {/* cart total price starts here  */}
+
+        <div className={Style.totalprice}>
+          <h3>Total: ${getTotalPrice()}</h3>
+          <button>Checkout</button>
+        </div>
+      </section>
     </>
   );
 };
